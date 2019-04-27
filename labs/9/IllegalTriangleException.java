08f81a09a7516b34aadfb61f5022b14a62215063
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2019-04-26 22:16:51 -0400

    Add files via upload

---
+++ labs/9/IllegalTriangleException.java
@@ -0,0 +1,14 @@
+package nineLabdushaj;
+
+public class IllegalTriangleException extends Exception{
+
+		public IllegalTriangleException() {
+			System.out.print("sum of two sides is grater than one side");
+			
+			
+			
+		}
+		}
+	
+
+
---
+++ labs/9/testTriangle.java
@@ -0,0 +1,12 @@
+package nineLabdushaj;
+
+public class testTriangle {
+
+	public static void main(String[] args) throws IllegalTriangleException{
+		Triangle e1 = new Triangle(2,3,10);
+		
+		
+		
+	}
+
+}
